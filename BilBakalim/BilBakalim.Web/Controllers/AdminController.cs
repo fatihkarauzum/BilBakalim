56c2edaee186e4fa33ff8347c6fd5a3d9cba0dd1
emre <me@kaymakemre.com>
2019-03-13 17:18:45 +0300

    kullanici işlem

--- BilBakalim/BilBakalim.Web/Controllers/AdminController.cs
+++ BilBakalim/BilBakalim.Web/Controllers/AdminController.cs
@@ -22,5 +22,62 @@ namespace BilBakalim.Web.Controllers
         {
             return View(db.Kullanici.ToList());
         }
+
+        [HttpGet]
+        public ActionResult KullaniciGuncelle(int id)
+        {
+            ViewBag.Roller = db.Rol.ToList();
+            Kullanici k = db.Kullanici.Where(x => x.ID == id).FirstOrDefault();
+            return View(k);
+
+        }
+
+        [HttpPost]
+        public ActionResult KullaniciGuncelle(Kullanici k)
+        {
+            try
+            {
+
+                Kullanici ku = db.Kullanici.Where(x => x.ID==k.ID).FirstOrDefault();
+                if (ku == null)
+                {
+                    return RedirectToAction("Hata", "Admin");
+                }
+                ku.Adi = k.Adi;
+                ku.Soyadi = k.Soyadi;
+                ku.Email = k.Email;
+                ku.Sifre = k.Sifre;              
+                db.SaveChanges();
+                TempData["GenelMesaj"] = "Kullanıcı güncelleme işlemi başarılı bir şekilde tamamlanmıştır.";
+                return RedirectToAction("KullaniciListesi");
+            }
+            catch (Exception)
+            {
+                return Redirect("/Admin/Hata");
+            }
+        }
+        [HttpPost]
+        public ActionResult KullaniciSil(int id)
+        {
+            Kullanici b = db.Kullanici.Where(x => x.ID == id).SingleOrDefault();
+            if (b == null)
+            {
+                return Json(false);
+            }
+            else
+            {
+                try
+                {
+                    db.Kullanici.Remove(b);
+                    db.SaveChanges();
+                    return Json(true);
+                }
+                catch (Exception)
+                {
+                    return Json("FK");
+                }
+
+            }
+        }
     }
 }
