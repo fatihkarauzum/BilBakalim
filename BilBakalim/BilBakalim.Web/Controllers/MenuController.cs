8295302328e394ef655416fa8c0fdbde40c8fac8
Fatih <fatih47154@gmail.com>
2019-03-23 19:03:07 +0300

    Menü İşlemleri

--- BilBakalim/BilBakalim.Web/Controllers/MenuController.cs
+++ BilBakalim/BilBakalim.Web/Controllers/MenuController.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data;
+using System.Data.Entity.Migrations;
+using System.Reflection;
 using BilBakalim.Data;
+using BilBakalim.Web.App_Classes;
 
 namespace BilBakalim.Web.Controllers
 {
     public class MenuController : Controller
     {
         BilBakalimContext db = new BilBakalimContext();
+
         // GET: Menu
         public ActionResult MenuListesi()
         {
@@ -41,7 +45,8 @@ namespace BilBakalim.Web.Controllers
                     if (menuRol.Count != 0)
                     {
                         return Json("menuRol");
-                    }else if (menuler.Count != 0)
+                    }
+                    else if (menuler.Count != 0)
                     {
                         return Json("menus");
                     }
@@ -58,5 +63,119 @@ namespace BilBakalim.Web.Controllers
 
             }
         }
+
+        [HttpGet]
+        public ActionResult MenuEkle()
+        {
+            var menus = db.Menu.Where(x => x.Aktif == true && x.Controller == null && x.Action == null).ToList();
+            ViewBag.menus = menus;
+
+            MvcHelper helper = new MvcHelper();
+
+            var controllerList = helper.GetControllerNames().ToList();
+            //var actionList = new ReflectedControllerDescriptor(typeof(ActionResult)).GetCanonicalActions();
+
+            ViewBag.controllers = controllerList;
+
+            //ViewBag.actions = actionList;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Ekle(Menu menu)
+        {
+            try
+            {
+                if (menu.Controller == "default")
+                {
+                    menu.Controller = null;
+                }
+                else if (menu.Controller != null)
+                {
+                    menu.Controller = menu.Controller.Substring(0, menu.Controller.Length - 10);
+                }
+                if (menu.Action == null)
+                {
+
+                }
+                if (menu.ParentMenuID != null)
+                {
+                    Menu parentMenu = db.Menu.FirstOrDefault(x => x.ID == menu.ParentMenuID);
+                    parentMenu.AcilirMenu = true;
+                    menu.AcilirMenu = false;
+                }
+                else
+                {
+                    menu.AcilirMenu = false;
+                }
+
+                menu.Aktif = true;
+                db.Menu.AddOrUpdate(menu);
+                db.SaveChanges();
+                TempData["GenelMesaj"] = "Menü ekleme işlemi başarılı bir şekilde tamamlanmıştır.";
+                return RedirectToAction("MenuListesi");
+            }
+            catch (Exception)
+            {
+                return Redirect("/Admin/Hata");
+            }
+        }
+        public ActionResult Duzenle(int id)
+        {
+            var menu = db.Menu.FirstOrDefault(x => x.Aktif == true && x.ID == id);
+            var menus = db.Menu.Where(x => x.Aktif == true && x.Controller == null && x.Action == null).ToList();
+
+            MvcHelper helper = new MvcHelper();
+
+            var controllerList = helper.GetControllerNames().ToList();
+            //var actionList = new ReflectedControllerDescriptor(typeof(ActionResult)).GetCanonicalActions();
+
+            ViewBag.controllers = controllerList;
+            ViewBag.menus = menus;
+
+            //ViewBag.actions = actionList;
+
+            return View(menu);
+        }
+
+        [HttpPost]
+        public ActionResult Duzenle(Menu menu)
+        {
+            try
+            {
+                if (menu.Controller == "default")
+                {
+                    menu.Controller = null;
+                }
+                else if(menu.Controller != null)
+                {
+                    menu.Controller = menu.Controller.Substring(0, menu.Controller.Length - 10);
+                }
+                //if (menu.Action == null)
+                //{
+
+                //}
+                if (menu.ParentMenuID != null)
+                {
+                    Menu parentMenu = db.Menu.FirstOrDefault(x => x.ID == menu.ParentMenuID);
+                    parentMenu.AcilirMenu = true;
+                }
+                else
+                {
+                    menu.AcilirMenu = false;
+                }
+
+                db.Menu.AddOrUpdate(menu);
+                db.SaveChanges();
+                TempData["GenelMesaj"] = "Menü Güncelleme işlemi başarılı bir şekilde tamamlanmıştır.";
+                return RedirectToAction("MenuListesi");
+            }
+            catch (Exception)
+            {
+                return Redirect("/Admin/Hata");
+            }
+        }
+
     }
 }
