1bcfe925f256bdf8bf4cf57c8c894252bf5c45cd
emre <emrekymk1996@gmail.com>
2019-03-28 21:07:06 +0300

    first data

---
+++ BilBakalim/BilBakalim.Data/Model/FirstDatas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilBakalim.Data.Model
+{
+    public class FirstDatas : IDatabaseInitializer<BilBakalimContext>
+    {
+        public void InitializeDatabase(BilBakalimContext context)
+        {
+            if (context.Menu.FirstOrDefault(x => x.Action == "MenuListesi" && x.Controller == "Menu") == null)
+            {
+                List<Menu> menu = new List<Menu>()
+                {
+                    new Menu(){Adi="Menü Listesi", AcilirMenu=false, Action="MenuListesi", Controller="Menu", Aktif=true, Icon="icon-group", ParentMenuID=null},
+                };
+                context.Menu.AddRange(menu);
+                context.SaveChanges();
+            }
+        }
+    }
+}
